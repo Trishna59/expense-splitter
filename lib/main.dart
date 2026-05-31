0a809003424826ee4fb67566d990de8da291fbdc
Trishna59 <lamsaltrishna@gmail.com>
2026-05-31 16:47:24 +0545

    chore: configure app entry point with Firebase and Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
+// main.dart
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/core/theme/theme.dart';
-import 'package:flutter_application_1/feature/auth/presentation/pages/signin_page.dart';
-import 'package:flutter_application_1/feature/auth/presentation/pages/signup_page.dart';
-import 'package:flutter_application_1/feature/auth/presentation/pages/verify_email_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';  
+import 'package:cloud_firestore/cloud_firestore.dart';       
+import 'package:Expenses_splitter/core/theme/theme.dart';
+import 'package:Expenses_splitter/feature/auth/presentation/pages/signin_page.dart';
+import 'package:Expenses_splitter/feature/auth/presentation/pages/verify_email_page.dart';
+import 'package:Expenses_splitter/feature/group/presentation/pages/groups_list_page.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,       // enable local cache
+    cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED, // no cache size limit
+  );
+  runApp(const ProviderScope(child: MyApp()));                     
 }
 
 class MyApp extends StatelessWidget {
@@ -19,30 +28,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Blog app',
+      title: 'Expense Splitter',
       theme: AppTheme.darkThemeMode,
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          // Firebase is still loading
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
+              body: Center(child: CircularProgressIndicator()),
             );
           }
-
-          // User is logged in
           if (snapshot.hasData) {
             if (snapshot.data!.emailVerified) {
-              return const SignupPage(); 
+              return const GroupsListPage();
             } else {
-              return const VerifyEmailPage(); // logged in but not verified
+              return const VerifyEmailPage();
             }
           }
-
-          // User is not logged in
           return const SignInPage();
         },
       ),
